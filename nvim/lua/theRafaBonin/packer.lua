75cc5907063b256185be9379d4346d8fb9d4bdb1
TheRafaBonin <rafabonin04@gmail.com>
2023-12-09 00:20:00 -0300

    adds packer bootstrap

--- nvim/lua/theRafaBonin/packer.lua
+++ nvim/lua/theRafaBonin/packer.lua
@@ -1,4 +1,16 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+      fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+      vim.cmd [[packadd packer.nvim]]
+      return true
+    end
+    return false
+  end
+
+  local packer_bootstrap = ensure_packer()
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -7,14 +19,14 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- Install telescope
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  use({ 'rose-pine/neovim', as = 'rose-pine' })
-
+  -- Install trouble
   use({
       "folke/trouble.nvim",
       config = function()
@@ -27,12 +39,7 @@ return require('packer').startup(function(use)
       end
   })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use("theprimeagen/refactoring.nvim")
-  use("tpope/vim-fugitive")
-  use("mbbill/undotree")
-  use("nvim-treesitter/nvim-treesitter-context")
-
+  -- Install LSP
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  branch = 'v3.x',
@@ -50,16 +57,29 @@ return require('packer').startup(function(use)
 	  }
   }
 
-  use("folke/zen-mode.nvim")
-  use("github/copilot.vim")
-  use("eandrju/cellular-automaton.nvim")
-  use("laytan/cloak.nvim")
-
+ -- Install nvim-tree
   use {
       'kyazdani42/nvim-tree.lua',
       requires = {
           'kyazdani42/nvim-web-devicons', -- optional, for file icons
       },
-      tag = 'nightly' -- optional, updated every week. 
   }
+
+  -- Install other plugins
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use({ 'rose-pine/neovim', as = 'rose-pine' })
+  use("nvim-treesitter/nvim-treesitter-context")
+  use("eandrju/cellular-automaton.nvim")
+  use("theprimeagen/refactoring.nvim")
+  use("folke/zen-mode.nvim")
+  use("github/copilot.vim")
+  use("tpope/vim-fugitive")
+  use("laytan/cloak.nvim")
+  use("mbbill/undotree")
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,1 +1,2 @@
+# !/usr/bin/env bash
 ln -s ~/dotfiles/nvim ~/.config/nvim
