6410214d1476d6f194ee5d279b4fe840907f1df5
TheRafaBonin <rafabonin04@gmail.com>
2023-12-09 01:20:29 -0300

    unwraps configuration

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,2 +1,11 @@
 # !/usr/bin/env bash
+ln -s ~/dotfiles/dunst ~/.config/dunst
+ln -s ~/dotfiles/hypr ~/.config/hypr
+ln -s ~/dotfiles/kitty ~/.config/kitty
 ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/rofi ~/.config/rofi
+ln -s ~/dotfiles/swappy ~/.config/swappy
+ln -s ~/dotfiles/tmux ~/.config/tmux
+ln -s ~/dotfiles/wal ~/.config/wal
+ln -s ~/dotfiles/waybar ~/.config/waybar
+ln -s ~/dotfiles/wlogout ~/.config/wlogout
